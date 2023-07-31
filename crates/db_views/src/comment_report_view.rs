d503833cc294acd901bf16b91f0ba9453fd33a2b
dullbananas <dull.bananas0@gmail.com>
2023-07-31 11:08:29 -0700

    Update comment_report_view.rs

--- crates/db_views/src/comment_report_view.rs
+++ crates/db_views/src/comment_report_view.rs
@@ -27,7 +27,7 @@ use lemmy_db_schema::{
     post,
   },
   source::{
-    comment::Comment,
+    comment::CommentWithoutId,
     comment_report::CommentReport,
     community::Community,
     person::Person,
@@ -66,15 +66,15 @@ fn queries<'a>() -> Queries<
 
   let selection = (
     comment_report::all_columns,
-    comment::all_columns,
-    post::all_columns,
-    community::all_columns,
-    person::all_columns,
-    aliases::person1.fields(person::all_columns),
+    CommentWithoutId::as_select(),
+    PostWithoutId::as_select(),
+    CommunityWithoutId::as_select(),
+    PersonWithoutId::as_select(),
+    aliases::person1.fields(PersonWithoutId::as_select()),
     CommentAggregatesNotInComment::as_select(),
     community_person_ban::id.nullable().is_not_null(),
     comment_like::score.nullable(),
-    aliases::person2.fields(person::all_columns).nullable(),
+    aliases::person2.fields(PersonWithoutId::as_select()).nullable(),
   );
 
   let read = move |mut conn: DbConn<'a>, (report_id, my_person_id): (CommentReportId, PersonId)| async move {
@@ -223,30 +223,41 @@ impl CommentReportQuery {
 impl JoinView for CommentReportView {
   type JoinTuple = (
     CommentReport,
-    Comment,
-    Post,
-    Community,
-    Person,
-    Person,
+    CommentWithoutId,
+    PostWithoutId,
+    CommunityWithoutId,
+    PersonWithoutId,
+    PersonWithoutId,
     CommentAggregatesNotInComment,
     bool,
     Option<i16>,
-    Option<Person>,
+    Option<PersonWithoutId>,
   );
 
   fn from_tuple(a: Self::JoinTuple) -> Self {
+    let comment_report = a.0;
     let counts = a.6.into_full(&a.1);
+    let comment = a.1.into_full(comment_report.comment_id);
+    let post = a.2.into_full(comment.post_id);
+    let community = a.3.into_full(post.community_id);
+    let creator = a.4.into_full(comment_report.creator_id);
+    let comment_creator = a.5.into_full(comment.creator_id);
+    let resolver = if let (Some(resolver), Some(resolver_id)) = (a.9, comment_report.resolver_id) {
+      Some(resolver.into_full(resolver_id))
+    } else {
+      None
+    };
     Self {
-      comment_report: a.0,
-      comment: a.1,
-      post: a.2,
-      community: a.3,
-      creator: a.4,
-      comment_creator: a.5,
+      comment_report,
+      comment,
+      post,
+      community,
+      creator,
+      comment_creator,
       counts,
       creator_banned_from_community: a.7,
       my_vote: a.8,
-      resolver: a.9,
+      resolver,
     }
   }
 }
