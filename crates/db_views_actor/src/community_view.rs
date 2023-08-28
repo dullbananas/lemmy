9fd923ffc67a37bcea5979278504cc2d5cca49bf
dullbananas <dull.bananas0@gmail.com>
2023-08-27 19:06:44 -0700

    Update community_view.rs

--- crates/db_views_actor/src/community_view.rs
+++ crates/db_views_actor/src/community_view.rs
@@ -11,17 +11,12 @@ use diesel::{
 };
 use diesel_async::RunQueryDsl;
 use lemmy_db_schema::{
-  aggregates::structs::CommunityAggregates,
   newtypes::{CommunityId, PersonId},
   schema::{community, community_aggregates, community_block, community_follower, local_user},
-  source::{
-    community::{Community, CommunityFollower},
-    local_user::LocalUser,
-  },
+  source::{community::CommunityFollower, local_user::LocalUser},
   utils::{fuzzy_search, limit_and_offset, DbConn, DbPool, ListFn, Queries, ReadFn},
   ListingType,
   SortType,
-  SubscribedType,
 };
 
 fn queries<'a>() -> Queries<
