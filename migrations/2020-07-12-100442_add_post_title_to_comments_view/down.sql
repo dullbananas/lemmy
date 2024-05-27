47822e9e477e2aff9dbfd1562ce634806cd127dd
dullbananas <dull.bananas0@gmail.com>
2024-05-27 14:54:24 -0700

    Update down.sql

--- migrations/2020-07-12-100442_add_post_title_to_comments_view/down.sql
+++ migrations/2020-07-12-100442_add_post_title_to_comments_view/down.sql
@@ -1,16 +1,16 @@
-DROP VIEW user_mention_view;
+DROP VIEW IF EXISTS user_mention_view;
 
-DROP VIEW reply_fast_view;
+DROP VIEW IF EXISTS reply_fast_view;
 
-DROP VIEW comment_fast_view;
+DROP VIEW IF EXISTS comment_fast_view;
 
-DROP VIEW comment_view;
+DROP VIEW IF EXISTS comment_view;
 
-DROP VIEW user_mention_fast_view;
+DROP VIEW IF EXISTS user_mention_fast_view;
 
-DROP TABLE comment_aggregates_fast;
+DROP TABLE IF EXISTS comment_aggregates_fast;
 
-DROP VIEW comment_aggregates_view;
+DROP VIEW IF EXISTS comment_aggregates_view;
 
 CREATE VIEW comment_aggregates_view AS
 SELECT
